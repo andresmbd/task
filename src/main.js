03e757eab0c1c325afb70dbe7f2b33b8e11b78e7
andresmbd <anmabadi@outlook.com>
2026-05-22 15:37:27 -0500

    creado comunicacion con json-server

--- src/main.js
+++ src/main.js
@@ -2,47 +2,49 @@ import './style.css'
 
 const appContainer = document.getElementById("app")
 
-async function getData(){
+async function getData() {
   const response = await fetch("http://localhost:3000/tasks")
   const data = await response.json()
-
   return data
 }
 
 async function postData(data) {
-  const response = await fetch("http://localhost:3000/tasks", {
-    method : 'POST',
-    headers : {
-      'Content-Type' : 'application/json' //We are going to send a Json
-    },
-    body : JSON.stringify(data) // but then we transform it into string
-  })
-  .then(response => {
-    if(!response.ok){
-    throw new Error('The response was not ok')
+  try {
+    const response = await fetch("http://localhost:3000/tasks", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json' //We are going to send a Json
+      },
+      body: JSON.stringify(data) // but then we transform it into string
+    })
+    if(!response.ok) throw new Error("dadada")
+    return await response.json()
+  } catch (error) {
+    console.log(error);
   }
-  return response.json() // we use return here in case the condition above is false, so we can canture it
-  })
-  // whatever is returned, the next .then.s will print the fail or success of response 
-  .then(result => {
-    console.log('Success!', result);
-  })
-  .catch(error => {
-    console.error('Error!', error);
-  })
+
 }
+
 // const addButtom = document.getElementById('add')
 const formTask = document.getElementById('formtask')
-formTask.addEventListener('submit', (e) => {
+formTask.addEventListener('submit', async (e) => {
   e.preventDefault()
+  const data = await getData()
+
+  console.log(data)
+
 
   const task = document.getElementById('task').value
 
   const dataTask = {
-    id : 3,
-    title : task,
-    completed : false
+    
+    title: task,
+    completed: false
   }
   postData(dataTask)
+  formTask.reset()
 
 })
+
+const editButtom = document.getElementById('title').textContent // title
+const checkbox = document.getElementById('checkbox').checked
